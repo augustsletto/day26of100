db4109134a59854cebdba5ff8492b9cbca115b70
augustsletto <augustsletto@gmail.com>
2024-11-11 16:21:24 +0100

    conditional list compr.

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ numbers = [1, 2, 3]
 new_list = [n + 1 for n in numbers]
 
 print(new_list)
+
+
+
+
+name = "August"
+
+new_list = [letter for letter in name]
+
+print(new_list)
+
+
+
+rng_list = [num * 2 for num in range(1, 5) if 5 == 5]
+
+print(rng_list)
